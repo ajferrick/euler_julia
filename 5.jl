49f4b112e50209989caf288752ce86812e12f62a
AJ Ferrick <ajferrick@gmail.com>
2016-02-09 02:58:21 -0800

    Problem 5

---
+++ 5.jl
@@ -0,0 +1,6 @@
+# Example
+# We can determine this by hand: from a range, remove numbers with redundant
+# division rules; e.g. 4 means that we can remove 2, and 8
+println(reduce(*, [10, 9, 7, 4]))
+println(reduce(*, [5, 9, 7, 11, 13, 16, 17, 19]))
+
